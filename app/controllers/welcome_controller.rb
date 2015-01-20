48ed982fadca7f7793c91420bf5d4ea65114471b
Ty Hyten <ty.hyten@gmail.com>
2015-01-20 14:51:51 -0700

    reformat quotes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-  end  
+    @quotes = [["gCamp has changed my life! It's the best tool I've ever used.","Cayla Hayes"],["Before gCamp I was a disorderly slob. Now I'm more organized than I've ever been", "Leta Jaskolski"],["Don't hesitate - sign up right now! You'll never be the same.","Lavern Upton"]]
+  end
 
 end
