5a0add5b9db097201748b32543d4fd8d063b1aa6
Ty Hyten <ty.hyten@gmail.com>
2015-02-03 19:12:28 -0700

    add users model, controller, form, pages

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+
+  def index
+    @users= User.all
+  end
+
+  def show
+    @users = User.find(params[:id])
+  end
+
+  def new
+    @users = User.new
+  end
+
+  def edit
+    @users = User.find(params[:id])
+  end
+
+  def create
+    @users = User.new(user_params)
+
+    if @users.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @users = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.destroy(params[:id])
+
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127215757) do
+ActiveRecord::Schema.define(version: 20150204001452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20150127215757) do
     t.datetime "updated_at",  null: false
     t.boolean  "complete"
     t.date     "date"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
   end
 
 end
