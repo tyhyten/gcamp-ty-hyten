06eddd52b6db25988dac0e1b80fc8d975f2a370b
Ty Hyten <ty.hyten@gmail.com>
2015-02-05 17:14:38 -0700

    add projects CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150204001452) do
+ActiveRecord::Schema.define(version: 20150205231815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
