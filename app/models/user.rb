9c92f50a4af646b3949b7c14f17f1b6b9f8bd9d4
Ty Hyten <ty.hyten@gmail.com>
2015-02-17 14:59:43 -0700

    Add project and users validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   "#{first_name} #{last_name}"
   end
 
-
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
 
 end
