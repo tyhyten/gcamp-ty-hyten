65f42986a2a0286f84b61bebf3e832ba640c9c58
Ty Hyten <ty.hyten@gmail.com>
2015-03-02 16:11:39 -0700

    update task_spec

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe 'User can CRUD tasks' do
 
   before :each do
+    @project = Project.create(name: "projecto")
     @task = Task.create(description: "tasks are for kids, silly rabbit")
     visit '/signup'
     fill_in 'user[first_name]', :with => "Bean"
@@ -12,23 +13,23 @@ describe 'User can CRUD tasks' do
     fill_in 'user[password_confirmation]', :with => "123456"
 
     click_on "Sign Up!"
-    visit '/tasks'
+    visit '/projects'
+    click_on "projecto"
+    click_on "New Task"
+    fill_in 'task[description]', :with => "vegan dog"
+    click_on "Create Task"
   end
 
   scenario 'User can create a task' do
-    click_on 'New Task'
-    fill_in 'task[description]', :with => "Eat some pudding!"
-    click_on "Create Task"
-    expect(page).to have_content "Task was successfully created."
+    expect(page).to have_content "vegan dog"
   end
 
   scenario 'User can view a task' do
-    click_on "tasks are for kids, silly rabbit"
-    expect(page).to have_content "tasks are for kids, silly rabbit"
+    click_on "Show"
+    expect(page).to have_content "vegan dog"
   end
 
   scenario 'User can update a task' do
-    click_on "tasks are for kids, silly rabbit"
     click_on "Edit"
     fill_in 'task[description]', :with => "hello grandmother"
     click_on "Update Task"
