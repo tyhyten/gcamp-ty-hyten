38296315de289457fb4ae1cec573e37f99a02778
Ty Hyten <ty.hyten@gmail.com>
2015-03-15 21:24:04 -0600

    clean up code

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,21 +2,16 @@ class TasksController < ApplicationController
   before_action :logged_in?
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
-  # GET /tasks
-  # GET /tasks.json
   def index
     @project = Project.find(params[:project_id])
     @tasks = @project.tasks
   end
 
-  # GET /tasks/1
-  # GET /tasks/1.json
   def show
     @task = Task.find(params[:id])
     @project = Project.find(params[:project_id])
   end
 
-  # GET /tasks/new
   def new
     @task = Task.new
     @project = Project.find(params[:project_id])
@@ -38,8 +33,6 @@ class TasksController < ApplicationController
       end
   end
 
-  # PATCH/PUT /tasks/1
-  # PATCH/PUT /tasks/1.json
   def update
     @project = Project.find(params[:project_id])
     if @task.update(task_params)
@@ -49,20 +42,16 @@ class TasksController < ApplicationController
     end
   end
 
-  # DELETE /tasks/1
-  # DELETE /tasks/1.json
   def destroy
     @task.destroy
       redirect_to project_tasks_path, notice: 'Task was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_task
       @task = Task.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
       params.require(:task).permit(:description, :complete, :date)
     end
