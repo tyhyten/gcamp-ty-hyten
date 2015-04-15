c325688ed8f47a7b3256276ebdf10e27cfb55047
Ty Hyten <ty.hyten@gmail.com>
2015-04-15 15:48:25 -0600

    redirect upon new session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,11 @@
 class SessionsController < ApplicationController
+  before_action :set_page
+
+  def set_page
+   unless request.referer.include?('/login')
+     session[:return_to] = request.referer
+   end
+  end
 
   def new
     @user = User.new
@@ -9,13 +16,17 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
       log_in @user
-      redirect_to projects_path , notice: 'User was successfully signed in'
+      redirect_to (session[:return_to]) , notice: 'User was successfully signed in'
     else
       @user = User.new # can delete this?
       @user.errors[:base] << ("User/Password incorrect")
       render 'new'
     end
   end
+
+  # redirect_to(session[:return_to] || default)
+  #     session[:return_to] = nil
+
 
   # def store_return_to
   #   session[:return_to] = request.fullpath
